a967cd95730b509f3323448ee17b99516cb0c77b
Jakob Jensen <jakobj@jakobj.dk>
2022-06-17 20:09:23 +0200

    Add reporting

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -29,7 +29,7 @@ which yq git > /dev/null
 
 echo -n 'Preparing runnner...'
 
-echo -n '' > $DIR/runner.sh
+echo 'TOTAL_PASS=0; TOTAL_FAIL=0' > $DIR/runner.sh
 
 yq -o json "$FILE" | jq -rc '.jobs | to_entries[] | [{group: .key, groupName: .value.name, script: .value.steps}] | .[]' | while read -r JOB; do
 
@@ -37,8 +37,9 @@ yq -o json "$FILE" | jq -rc '.jobs | to_entries[] | [{group: .key, groupName: .v
     GROUP_NAME=$(echo "$JOB" | jq -rj '.groupName')
 
     echo "cd '${DIR}/workspace/'; git reset HEAD --hard > /dev/null; git clean -fdx . > /dev/null; sh '${DIR}/${GROUP}.sh'" >> $DIR/runner.sh
+    echo "TOTAL_PASS=\$((TOTAL_PASS+\$(cat ${DIR}/PASS))); TOTAL_FAIL=\$((TOTAL_FAIL+\$(cat ${DIR}/FAIL)))" >> $DIR/runner.sh
     echo "echo; echo '$GROUP_NAME'" > "${DIR}/${GROUP}.sh"
-    echo "GREEN=$'\e[0;32m'; RED=$'\e[0;31m'; NC=$'\e[0m'" >> "${DIR}/${GROUP}.sh"
+    echo "GREEN=$'\e[0;32m'; RED=$'\e[0;31m'; NC=$'\e[0m'; PASS=0; FAIL=0" >> "${DIR}/${GROUP}.sh"
 
     I=0
     echo "$JOB" | jq -rc '.script[] | select(.run != null)' | while read -r STEP; do
@@ -49,12 +50,17 @@ yq -o json "$FILE" | jq -rc '.jobs | to_entries[] | [{group: .key, groupName: .v
 
         echo "echo -n ' - ${STEP_NAME}: '" >> "${DIR}/${GROUP}.sh"
         echo "set +e; sh '${DIR}/${GROUP}.${I}.sh' > messages 2>&1" >> "${DIR}/${GROUP}.sh"
-        echo "if [ \$? -eq 0 ]; then echo -e \${GREEN}'Pass'\${NC}; else echo -e \${RED}'Fail'\${NC}; cat messages; fi" >> "${DIR}/${GROUP}.sh"
+        echo "if [ \$? -eq 0 ]; then echo -e \${GREEN}'Pass'\${NC}; PASS=\$((PASS+1)); else echo -e \${RED}'Fail'\${NC}; FAIL=\$((FAIL+1)); cat messages; fi;" >> "${DIR}/${GROUP}.sh"
 
         I=$((I+1))
     done
 
+    echo "echo -n \$PASS > '${DIR}/PASS'; echo -n \$FAIL > '${DIR}/FAIL'" >> "${DIR}/${GROUP}.sh"
+
 done
+
+echo 'TOTAL=$((TOTAL_PASS+TOTAL_FAIL))' >> $DIR/runner.sh
+echo 'echo; echo -e "Tests; Total: \033[1m${TOTAL}\033[0m Passes: \033[1m${TOTAL_PASS}\033[0m Fails: \033[1m${TOTAL_FAIL}\033[0m"' >> $DIR/runner.sh
 
 echo ' done!'
 
