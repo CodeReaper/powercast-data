3925aa9a8a498adee3e940c08f45e300e52734fc
Jakob Jensen <jakobj@jakobj.dk>
2022-07-17 14:56:39 +0200

    Adapt to new apis (#27)

--- .github/scripts/co2-emission-data-pull.sh
+++ .github/scripts/co2-emission-data-pull.sh
@@ -23,11 +23,13 @@
 
 set -e
 
+OFFSET=0
+LIMIT=50
 AREA=$1
 ENDDATE=$2
 DIR=/tmp/$$
 ENDPOINT=https://api.energidataservice.dk/
-QUERY="datastore_search?resource_id=co2emisprog&limit=50&sort=Minutes5UTC%20desc&fields=Minutes5UTC,PriceArea,CO2Emission&include_total=false&records_format=objects"
+QUERY="dataset/co2emisprog?limit=${LIMIT}&sort=Minutes5UTC%20desc&columns=Minutes5UTC,PriceArea,CO2Emission"
 
 which mkdir wget jq cat date > /dev/null
 
@@ -36,19 +38,21 @@ trap 'set +x; rm -fr $DIR >/dev/null 2>&1' 0
 trap 'exit 2' 1 2 3 15
 
 CURSORDATE=$(date -d +14days +"%s")
-REQUEST=$(echo "{\"filters\":{\"PriceArea\":\"${AREA}\"}}" | jq -r "@uri \"${ENDPOINT}${QUERY}&filters=\(.filters)\"")
+TEMPLATE=$(echo "{\"PriceArea\":\"${AREA}\"}" | jq -r "@uri \"${ENDPOINT}${QUERY}&filter=\(.)\"")
 
 echo '[]' > $DIR/data.json
 while [ $CURSORDATE -gt $ENDDATE ]; do
+    REQUEST="${TEMPLATE}&offset=${OFFSET}"
+
     wget -nv -O $DIR/request.json "${REQUEST}"
 
-    TRANSFORMATION='.result.records |= map(.co2 = .CO2Emission | .timestamp = .Minutes5UTC | del(.Minutes5UTC, .CO2Emission, .PriceArea)) | .result.records[].timestamp |= (split("+")[0] + "Z"|fromdateiso8601) | .result.records'
+    TRANSFORMATION='.records |= map(.co2 = .CO2Emission | .timestamp = .Minutes5UTC | del(.Minutes5UTC, .CO2Emission, .PriceArea)) | .records[].timestamp |= (split("+")[0] + "Z"|fromdateiso8601) | .records'
     cat $DIR/request.json | jq -r "$TRANSFORMATION" > $DIR/data.new.json
     jq -s '.[0] + .[1]' $DIR/data.json $DIR/data.new.json > $DIR/data.combined.json
     mv -f $DIR/data.combined.json $DIR/data.json
 
     CURSORDATE=$(cat $DIR/data.json | jq -r 'map(.timestamp | values) | min')
-    REQUEST=${ENDPOINT}$(cat $DIR/request.json | jq -r '.result._links.next')
+    OFFSET=$((OFFSET+LIMIT))
 
     if [ "$CURSORDATE" = "null" ]; then
         CURSORDATE=0
--- .github/scripts/energy-price-data-pull.sh
+++ .github/scripts/energy-price-data-pull.sh
@@ -23,11 +23,13 @@
 
 set -e
 
+OFFSET=0
+LIMIT=50
 AREA=$1
 ENDDATE=$2
 DIR=/tmp/$$
 ENDPOINT=https://api.energidataservice.dk/
-QUERY="datastore_search?resource_id=elspotprices&limit=50&sort=HourUTC%20desc&fields=HourUTC,PriceArea,SpotPriceEUR&include_total=false&records_format=objects"
+QUERY="dataset/elspotprices?limit=${LIMIT}&sort=HourUTC%20desc&columns=HourUTC,PriceArea,SpotPriceEUR"
 
 which mkdir wget jq cat date > /dev/null
 
@@ -36,19 +38,21 @@ trap 'set +x; rm -fr $DIR >/dev/null 2>&1' 0
 trap 'exit 2' 1 2 3 15
 
 CURSORDATE=$(date -d +14days +"%s")
-REQUEST=$(echo "{\"filters\":{\"PriceArea\":\"${AREA}\"}}" | jq -r "@uri \"${ENDPOINT}${QUERY}&filters=\(.filters)\"")
+TEMPLATE=$(echo "{\"PriceArea\":\"${AREA}\"}" | jq -r "@uri \"${ENDPOINT}${QUERY}&filter=\(.)\"")
 
 echo '[]' > $DIR/data.json
 while [ $CURSORDATE -gt $ENDDATE ]; do
+    REQUEST="${TEMPLATE}&offset=${OFFSET}"
+
     wget -nv -O $DIR/request.json "${REQUEST}"
 
-    TRANSFORMATION='.result.records |= map(.euro = .SpotPriceEUR | .timestamp = .HourUTC | del(.SpotPriceEUR, .HourUTC, .PriceArea)) | .result.records[].timestamp |= (split("+")[0] + "Z"|fromdateiso8601) | .result.records'
+    TRANSFORMATION='.records |= map(.euro = .SpotPriceEUR | .timestamp = .HourUTC | del(.SpotPriceEUR, .HourUTC, .PriceArea)) | .records[].timestamp |= (split("+")[0] + "Z"|fromdateiso8601) | .records'
     cat $DIR/request.json | jq -r "$TRANSFORMATION" > $DIR/data.new.json
     jq -s '.[0] + .[1]' $DIR/data.json $DIR/data.new.json > $DIR/data.combined.json
     mv -f $DIR/data.combined.json $DIR/data.json
 
     CURSORDATE=$(cat $DIR/data.json | jq -r 'map(.timestamp | values) | min')
-    REQUEST=${ENDPOINT}$(cat $DIR/request.json | jq -r '.result._links.next')
+    OFFSET=$((OFFSET+LIMIT))
 
     if [ "$CURSORDATE" = "null" ]; then
         CURSORDATE=0
