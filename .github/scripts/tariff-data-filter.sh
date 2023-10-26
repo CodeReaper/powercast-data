6a230b04928c8904cab41ca1ae2fda4264dfd4e8
Jakob Jensen <jakobj@jakobj.dk>
2023-10-26 21:26:32 +0200

    Setup repeated solution

--- .github/scripts/tariff-data-filter.sh
+++ .github/scripts/tariff-data-filter.sh
@@ -69,12 +69,15 @@ jq -rc --arg zone "$AREA" '.[] | select(.zone == $zone) | .networks[]' < "$CONFI
     TRANSFORMATION='. |= map(.from = (.ValidFrom + "Z"|fromdateiso8601) | .to = if (.ValidTo|type) == "object" then null else (.ValidTo + "Z"|fromdateiso8601) end | del(.ValidFrom, .ValidTo))'
     UPDATE='[.[] | if (.Price2 == 0 and .Price3 == 0 and .Price4 == 0 and .Price5 == 0 and .Price6 == 0 and .Price7 == 0 and .Price8 == 0 and .Price9 == 0 and .Price10 == 0 and .Price11 == 0 and .Price12 == 0 and .Price13 == 0 and .Price14 == 0 and .Price15 == 0 and .Price16 == 0 and .Price17 == 0 and .Price18 == 0 and .Price19 == 0 and .Price20 == 0 and .Price21 == 0 and .Price22 == 0 and .Price23 == 0 and .Price24 == 0) then (. | .Price2 = .Price1 | .Price3 = .Price1 | .Price4 = .Price1 | .Price5 = .Price1 | .Price6 = .Price1 | .Price7 = .Price1 | .Price8 = .Price1 | .Price9 = .Price1 | .Price10 = .Price1 | .Price11 = .Price1 | .Price12 = .Price1 | .Price13 = .Price1 | .Price14 = .Price1 | .Price15 = .Price1 | .Price16 = .Price1 | .Price17 = .Price1 | .Price18 = .Price1 | .Price19 = .Price1 | .Price20 = .Price1 | .Price21 = .Price1 | .Price22 = .Price1 | .Price23 = .Price1 | .Price24 = .Price1) else . end]'
     MERGE='. |= map(.tariffs = [.Price1, .Price2, .Price3, .Price4, .Price5, .Price6, .Price7, .Price8, .Price9, .Price10, .Price11, .Price12, .Price13, .Price14, .Price15, .Price16, .Price17, .Price18, .Price19, .Price20, .Price21, .Price22, .Price23, .Price24] | del(.Price1, .Price2, .Price3, .Price4, .Price5, .Price6, .Price7, .Price8, .Price9, .Price10, .Price11, .Price12, .Price13, .Price14, .Price15, .Price16, .Price17, .Price18, .Price19, .Price20, .Price21, .Price22, .Price23, .Price24))'
+    # shellcheck disable=SC2016
+    GROUP='group_by(.tariffs) | map(reduce .[] as $item ({}; .from = if (.from == null or $item.from < .from) then $item.from else .from end, .to = if (.to == null or $item.to > .to) then $item.to else .to end, .from = $item.from | .to = $item.to | .tariffs = $item.tariffs)) | if (. | length) > 0 then .[0].to = null else . end'
 
     jq -r --arg id "$id" --arg type "$type" --arg code "$code" '[.[] | select(.GLN_Number == $id) | select(.ChargeType == $type) | select(.ChargeTypeCode == $code)]' < "$INPUT" | \
     jq -r "$CLEANUP" | \
     jq -r "$TRANSFORMATION" | \
     jq -r "$UPDATE" | \
     jq -r "$MERGE" | \
+    jq -r "$GROUP" | \
     jq -r --arg id "$id" --arg name "$name" '[{id: $id|tonumber, name: $name, tariffs: .}]' > "$DIR/$id.json"
 done
 
