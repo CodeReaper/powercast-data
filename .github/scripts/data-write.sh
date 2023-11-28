aa90e01f4b2d52fe071cf9fa1675fac13263b58e
Jakob Jensen <jakobj@jakobj.dk>
2023-11-28 22:16:17 +0100

    Apply faster writing

--- .github/scripts/data-write.sh
+++ .github/scripts/data-write.sh
@@ -23,34 +23,32 @@ set -e
 FILE=$1
 FOLDER=$2
 AREA=$3
-DIR=/tmp/$$
 
 which mkdir jq tr cut dirname > /dev/null
-
-mkdir -p $DIR
-trap 'set +x; rm -fr $DIR >/dev/null 2>&1' 0
-trap 'exit 2' 1 2 3 15
 
 [ -f "$FILE" ] || { echo "Not a file: $FILE"; exit 1; }
 [ -d "$FOLDER" ] || { echo "Not a directory: $FOLDER"; exit 2; }
 AREA=$(echo "$AREA" | tr '[:lower:]' '[:upper:]')
 [ -z "$AREA" ] && { echo "Invalid/Missing area."; exit 3; }
 
-jq -rc '.[]' "$FILE" | while read -r ITEM; do
-    echo "[" > $DIR/item.json
-    echo "$ITEM" >> $DIR/item.json
-    echo "]" >> $DIR/item.json
-
-    DATE=$(echo "$ITEM" | jq -r '.timestamp | todate' | cut -dT -f1)
+jq -rc '.[] | .data = . | .date = (.timestamp | todate) | .date, .data' "$FILE" | while read -r DATE; do
+    read -r ITEM
+    TIME=$(echo "$DATE" | cut -dT -f2 | cut -d ':' -f1-2 | sed s/://g)
+    DATE=$(echo "$DATE" | cut -dT -f1)
     YEAR=$(echo "$DATE" | cut -d- -f1)
     MONTH=$(echo "$DATE" | cut -d- -f2)
     DAY=$(echo "$DATE" | cut -d- -f3)
-    DESTINATION="${FOLDER}/${YEAR}/${MONTH}/${DAY}/${AREA}.json"
+    DESTINATION="${FOLDER}/${YEAR}/${MONTH}/${DAY}/${AREA}-${TIME}.json"
 
-    mkdir -p "$(dirname "$DESTINATION")"
-    if [ ! -f "$DESTINATION" ]; then
-        echo '[]' > "$DESTINATION"
+    if [ ! -d "${FOLDER}/${YEAR}/${MONTH}/${DAY}" ]; then
+        mkdir -p "$(dirname "$DESTINATION")"
     fi
-    jq -s '.[1] + .[0] | unique_by(.timestamp) | sort_by(.timestamp)' "$DIR/item.json" "$DESTINATION" > "$DIR/combined.json"
-    mv -f "$DIR/combined.json" "$DESTINATION"
+    echo "$ITEM" > "$DESTINATION"
+done
+
+for DESTINATION in "$FOLDER"/????/??/??; do
+  [ -d "$DESTINATION" ] || continue
+  ls "${DESTINATION}/${AREA}"-*.json >/dev/null 2>&1 || continue
+  find "${DESTINATION}" -type f \( -name "${AREA}.json" -o -name "${AREA}-*.json" \) -print0 | xargs -0 jq -s '. | unique_by(.timestamp) | sort_by(.timestamp)' > /tmp/$$.json && mv /tmp/$$.json "${DESTINATION}/${AREA}.json"
+  rm "${DESTINATION}/${AREA}"-*.json
 done
