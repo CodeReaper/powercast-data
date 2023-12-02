25491b9b6a6f224b73e6fb0f178882df479c9f39
Jakob Jensen <jakobj@jakobj.dk>
2023-12-02 15:09:37 +0100

    Add period pulling (#67)

--- .github/scripts/data-matrix.sh
+++ .github/scripts/data-matrix.sh
@@ -17,8 +17,13 @@
 #       Examples:
 #           - ./
 #           - data/
-#   - timestamp:
-#       Optional: timestamp to override the latest value
+#   - from timestamp:
+#       Optional: timestamp to override the latest value, note this requires the end timestamp to be set
+#       Examples:
+#           - 1654041600
+#           - 1701302400
+#   - end timestamp:
+#       Optional: timestamp to override the end value, note this requires the from timestamp to be set
 #       Examples:
 #           - 1654041600
 #           - 1701302400
@@ -27,7 +32,8 @@
 # [
 #   {
 #     "zone": "DK2",
-#     "latest": 1654041600
+#     "latest": 1654041600,
+#     "end": 1654041600
 #   },
 #   ...
 # ]
@@ -35,7 +41,8 @@
 CONFIG=$1
 CATEGORY=$2
 FOLDER=$3
-OVERRIDEN=$([ -n "$4" ] && printf "%d" "$4" 2>/dev/null)
+FROM=$([ -n "$4" ] && printf "%d" "$4" 2>/dev/null)
+END=$([ -n "$5" ] && printf "%d" "$5" 2>/dev/null)
 
 set -e
 which dirname jq sed > /dev/null
@@ -45,20 +52,24 @@ SCRIPTS=$(dirname "$0")
 [ -f "$CONFIG" ] || { echo "Not a file: $CONFIG"; exit 1; }
 [ -z "$CATEGORY" ] && { echo "Missing capability"; exit 2; }
 [ -d "$FOLDER" ] || { echo "Not a directory: $FOLDER"; exit 3; }
-if [ "$OVERRIDEN" = "0" ] && [ -n "$4" ]; then
+if [ "$FROM" = "0" ] && [ -n "$4" ]; then
   echo "Not a number: $4"
   exit 4
 fi
+if [ "$END" = "0" ] && [ -n "$5" ]; then
+  echo "Not a number: $5"
+  exit 5
+fi
+END=${END:-$(date -d +14days +"%s")}
 
-{
-  printf '['
-  jq -rc --arg category "$CATEGORY" '.[] | select(.capabilities | index($category) | .)' "$CONFIG" | while read -r ITEM; do
-      AREA=$(echo "$ITEM" | jq -r '.zone')
-      LATEST=$(sh "${SCRIPTS}/data-freshness.sh" "$FOLDER" "$AREA" 0)
-      if [ -n "$OVERRIDEN" ]; then
-        LATEST="$OVERRIDEN"
-      fi
-      printf "{\"zone\":\"%s\",\"latest\":%s}," "$AREA" "$LATEST"
-  done
-  printf ']'
-} | sed 's|,]|]|g' | jq -r '.'
+# shellcheck disable=SC2166
+if [ -z "$4" -a -n "$5" ] || [ -n "$4" -a -z "$5" ]; then
+  exit 6
+fi
+
+jq -rc --arg category "$CATEGORY" '.[] | select(.capabilities | index($category) | .) | .zone' "$CONFIG" | while read -r AREA; do
+  if [ -z "$FROM" ]; then
+    FROM=$(sh "${SCRIPTS}/data-freshness.sh" "$FOLDER" "$AREA" 0)
+  fi
+  printf "{\"zone\":\"%s\",\"latest\":%s,\"end\":%s}\n" "$AREA" "$FROM" "$END"
+done | jq -src '.'
