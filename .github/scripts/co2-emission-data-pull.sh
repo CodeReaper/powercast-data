26a710af28dbc14483a019a9e0fec81199c742f0
Jakob Jensen <jakobj@jakobj.dk>
2023-12-02 14:55:51 +0100

    Add period pulling

--- .github/scripts/co2-emission-data-pull.sh
+++ .github/scripts/co2-emission-data-pull.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-# Takes two arguments:
+# Takes three arguments:
 #   - price area:
 #       Data will be restricted to this price area.
 #       Must be a singular valid area.
 #       Examples:
 #           - DK1
 #           - DE
+#   - from date:
+#       Data will be fetched between the from and end date.
+#       Must be a unix timestamp.
+#       Example: 1654012800
 #   - end date:
-#       Data will be fetched until the end date is reached.
+#       Data will be fetched between the from and end date.
 #       Must be a unix timestamp.
 #       Example: 1654012800
 
@@ -24,12 +28,13 @@
 set -e
 
 OFFSET=0
-LIMIT=50
+LIMIT=100
 AREA=$1
-ENDDATE=$2
+FROMDATE=$2
+ENDDATE=$3
 DIR=/tmp/$$
 ENDPOINT=https://api.energidataservice.dk/
-QUERY="dataset/co2emisprog?limit=${LIMIT}&sort=Minutes5UTC%20desc&columns=Minutes5UTC,PriceArea,CO2Emission"
+QUERY="dataset/co2emisprog?limit=${LIMIT}&sort=Minutes5UTC%20desc&columns=Minutes5UTC,PriceArea,CO2Emission&start=$(date -d "@$FROMDATE" +"%Y-%m-%dT%H:%M")&end=$(date -d "@$ENDDATE" +"%Y-%m-%dT%H:%M")&timezone=UTC"
 
 which mkdir wget jq cat date > /dev/null
 
@@ -37,11 +42,10 @@ mkdir -p $DIR
 trap 'set +x; rm -fr $DIR >/dev/null 2>&1' 0
 trap 'exit 2' 1 2 3 15
 
-CURSORDATE=$(date -d +14days +"%s")
 TEMPLATE=$(echo "{\"PriceArea\":\"${AREA}\"}" | jq -r "@uri \"${ENDPOINT}${QUERY}&filter=\(.)\"")
 
 echo '[]' > $DIR/data.json
-while [ "$CURSORDATE" -gt "$ENDDATE" ]; do
+while true; do
     REQUEST="${TEMPLATE}&offset=${OFFSET}"
 
     wget -nv -O $DIR/request.json "${REQUEST}"
@@ -49,15 +53,13 @@ while [ "$CURSORDATE" -gt "$ENDDATE" ]; do
     # shellcheck disable=SC2016
     TRANSFORMATION='.records |= map(. as $item | {co2: $item.CO2Emission, timestamp: $item.Minutes5UTC}) | .records[].timestamp |= (split("+")[0] + "Z"|fromdateiso8601) | .records'
     cat $DIR/request.json | jq -r "$TRANSFORMATION" > $DIR/data.new.json
+
+    jq -e 'if . == [] then false else true end' < $DIR/data.new.json > /dev/null || break
+
     jq -s '.[0] + .[1]' $DIR/data.json $DIR/data.new.json > $DIR/data.combined.json
     mv -f $DIR/data.combined.json $DIR/data.json
 
-    CURSORDATE=$(cat $DIR/data.json | jq -r 'map(.timestamp | values) | min')
     OFFSET=$((OFFSET+LIMIT))
-
-    if [ "$CURSORDATE" = "null" ]; then
-        CURSORDATE=0
-    fi
 done
 
 cat $DIR/data.json
